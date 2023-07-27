b24f2522f2c4f5fdbfe314b3c1d9bfd8c8869d63
alisaad-dev1929 <140160920+alisaad-dev1929@users.noreply.github.com>
2023-07-27 07:15:42 +0300

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+$(".radio-btn").click(function () {
+$(".radio-inner").toggleClass("test");
+$("body").toggleClass("dark");
+})
+
+})
+
+function homeFunction() {
+    window.location.href = "home.html";
+}
